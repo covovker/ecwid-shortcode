05c27723b9e54ba8c1e2c648337ce6d8c8adfa21
vuvvn <vuvvn@kde205-red.crtdev.local>
2013-08-21 18:02:21 +0400

    Renamed to ecwid shopping cart hosted to avoid wordpress thinking it is the same ecwid-shopping-cart plugin

--- ecwid-shopping-cart-hosted/ecwid-shopping-cart-hosted.php
+++ ecwid-shopping-cart-hosted/ecwid-shopping-cart-hosted.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Ecwid Shopping Cart
+Plugin Name: Ecwid Shopping Cart Hosted
 Plugin URI: http://www.ecwid.com/ 
 Description: Ecwid is a free full-featured shopping cart. It can be easily integreted with any Wordpress blog and takes less than 5 minutes to set up.
 Author: Ecwid Team
